8c67e0375d383c5964f199e0b131e9254f65429f
nwnordahl <nicolaiwnordahl@gmail.com>
2021-07-29 14:59:40 +0200

    add stack class

--- Deck.py
+++ Deck.py
@@ -26,7 +26,7 @@ class Deck:
     def pull(self):
         """Return a card and remove it from the deck."""
         card = self.cards[0]
-        self.cards = self.cards[1:]
+        del self.cards[0]
         return card
 
     def show(self):
@@ -37,6 +37,29 @@ class Deck:
 
     def size(self):
         """Return amount of remaining cards in the deck."""
+        return len(self.cards)
+
+
+class Stack:
+    """Stack class."""
+
+    def __init__(self):
+        self.cards = []
+
+    def previous_card(self):
+        """Return most recently added card."""
+        return self.cards[-1]
+
+    def add(self, card):
+        """Add a card to the stack."""
+        self.cards.append(card)
+
+    def empty(self):
+        """Empty the stack."""
+        self.cards = []
+
+    def size(self):
+        """Return the amount of cards in the stack."""
         return len(self.cards)
 
 
